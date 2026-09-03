1bc6a520dfccf695a2c91d081903628021ee1b4f
rovermdashfaq <rovermdashfaq@gmail.com>
2026-09-03 19:56:56 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 // sw.js - Service Worker for Web Push Notifications
 
 // Cache name for static assets
-const CACHE_NAME = 'blood-donor-bd-v2.0.3';
+const CACHE_NAME = 'blood-donor-bd-v2.0.4';
 
 // Install event - cache static assets
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        '/',
-        '/bh.html',
+        '/database',
+        '/index.html',
         'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
       ]).catch(error => {
